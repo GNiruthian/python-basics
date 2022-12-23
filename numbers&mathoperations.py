8d853da843eaece91cbd675a880818646f32a3ec
G.Niruthian <88297426+GNiruthian@users.noreply.github.com>
2022-12-23 09:30:23 +0530

    Add files via upload

---
+++ numbers&mathoperations.py
@@ -0,0 +1,54 @@
+a= 15
+b= 12.5
+c= 5j
+
+print(type(a))
+print(type(b))
+print(type(c))
+
+print(type(6.988))
+print(type(35))
+
+# convert
+
+#convert from float to int
+n= int(b)
+
+#convert from int to float
+i=float(a)
+
+#convert from int to complex
+r=complex(a)
+
+print(n)
+print(i)
+print(r)
+
+
+x=25
+y=34
+
+w=(x+y)
+v=(x-y)
+o=(y*x)
+p=(y/x)
+q=(y//x)
+t=(x**y)
+
+
+print(w)
+print(v)
+print(p)
+print(q)
+print(t)
+
+'''
+Addition +
+Subtraction -
+Multiplication *
+Division /
+Modulus %
+Exponential **
+Floor division //
+
+'''
