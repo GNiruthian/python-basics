d843b5dab18f396c6c2a8de18efc6faaf31b5be0
G.Niruthian <88297426+GNiruthian@users.noreply.github.com>
2023-01-03 10:18:40 +0530

    Add files via upload

---
+++ casting.py
@@ -0,0 +1,14 @@
+n = 15.15
+print(n)
+print(type(n))
+
+a = int(n)
+print(a)
+print(type(a))
+
+b = int(input("Enter a number : "))
+c = int(input("Enter a number : "))
+d = b+c
+
+print("Total ", d)
+print("Total : " + str(d))
