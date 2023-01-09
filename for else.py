44656422dbf35da146de21eff6c385a2feb50434
G.Niruthian <88297426+GNiruthian@users.noreply.github.com>
2023-01-09 19:15:44 +0530

    Add files via upload

---
+++ for else.py
@@ -0,0 +1,11 @@
+for i in range(1,15):
+    print(i)
+else:
+    print("for loop end! :)")
+
+for n in range(1,15):
+    if n == 5:
+        break
+    print(n)
+else:
+    print("for loop end :)))")
