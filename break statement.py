a3f147ac70ec037b44d3476ada6e3bf1c72ae514
G.Niruthian <88297426+GNiruthian@users.noreply.github.com>
2023-01-09 19:16:29 +0530

    Add files via upload

---
+++ break statement.py
@@ -0,0 +1,6 @@
+i = 1
+while i <= 20:
+    if i == 7:
+        break
+    print(i)
+    i += 1
---
+++ range statement.py
@@ -0,0 +1,8 @@
+n = list(range(0,5))
+print(n)
+#print even number
+print(list(range(0,15,2)))
+#print odd number
+print(list(range(1,15,2)))
+
+print(list(range(0,5)))
